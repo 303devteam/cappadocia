6b4e183790ceff4e84600f842b413b70b7d25c23
naimPJ <private.naim@gmail.com>
2024-01-22 09:39:35 +0100

    advanced - why us section closed

---
+++ app/components/contactsUs.js
@@ -0,0 +1,23 @@
+'use client';
+import styled from "styled-components";
+import Image from "next/image";
+import {Title} from './welcomeSection';
+import {Paragraph} from './aboutUs';
+
+const ContactUs = styled.div`
+    max-width: 100%;
+    background-color: white ;
+
+ 
+`;
+
+export default function contactUs() {
+    return (
+        <>
+            <ContactUs>
+            
+            </ContactUs>
+        </>
+    );
+}
+
